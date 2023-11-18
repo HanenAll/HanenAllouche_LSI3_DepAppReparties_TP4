4ec9e41386a0790bd67847225ff69e10c82039dd
Hanen Allouche <106692503+HanenAll@users.noreply.github.com>
2023-11-18 23:01:32 +0100

    upload files

--- src/client/Client3.java
+++ src/client/Client3.java
@@ -26,6 +26,16 @@ public class Client3 {
 			DatagramPacket sendPacket=new DatagramPacket(sendData,sendData.length,inetAddress,PORT); /* on met les données dans un packet et on specifie 
 			l'@ de la machine et le port */
 			socket.send(sendPacket); // envoit du packet au serveur
+
+			//reception des messages des autres clients
+			DatagramPacket receivePacket = new DatagramPacket(receiveData,receiveData.length); //on prepare le Packet vide pour la reception des données
+			socket.receive(receivePacket); // la reception  d'un datagram envoyer par le serveur et la lecture des octects qui se trouve dans le port 
+			// la methode receive necessite dans ses param un packet vide
+			String message= new String(receivePacket.getData(),0,receivePacket.getLength()); /* on recupere le message envoyer par le serveur 
+			et on fait sa conversion de byte en String par le constructeur du String , la methode getData() elle me donne le tableau d'octect ,
+			 0 c'est l'indice de depart et reveivePakcet.getlength() c'est l'indice de fin , on faot ici on va lire et convertir le tableau du premier
+			  indice jusq'au sa fin */
+			
 			socket.close();// fermeture du socket
 		} catch (Exception e) {
 			e.printStackTrace();
